7ccc8cf103b495d0fbf78b2966c49d426d66d711
Het Patel <48525849+hetp30@users.noreply.github.com>
2026-08-28 18:47:10 +0530

    Add Q1.java

---
+++ Q1.java
@@ -0,0 +1,18 @@
+public class Q1 {
+
+    public static void main(String[] args) {
+
+        int sum = 0;
+
+        for (int i = 1; i < 1000; i++) {
+
+            if (i % 3 == 0 || i % 5 == 0) {
+
+                sum += i;
+            }
+        }
+
+        //sum
+        System.out.println("Sum: " + sum);
+    }
+}
